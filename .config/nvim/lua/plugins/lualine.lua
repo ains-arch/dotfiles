12df328bb893d60a58fb3d2a56d16a3aa875366e
ains-arch <ainsleearchibald@gmail.com>
2024-07-22 14:35:54 -0600

    changed theme

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,6 +3,7 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
+    vim.cmd("colorscheme nightfox")
     require('lualine').setup()
   end
 }
